e3daaee542ac00383b6709d085da873468b85a2e
MarkTocino <Mark_tocino@yahoo.com>
2023-11-14 11:16:52 -0500

    Testing Added Sessions

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.use(
     cors({
         credentials:true,
-        origin: ['http://localhost:3000','https://honda-e-commerce.vercel.app', 'https://enthusiastic-puce-dove.cyclic.app/']
+        origin: ['http://localhost:3000','https://honda-e-commerce.vercel.app']
     })
 )
 router.get('/',nothing )
