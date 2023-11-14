61a5d18f37a3f6720fa029eb24e6be8f875ac196
MarkTocino <Mark_tocino@yahoo.com>
2023-11-14 02:43:26 -0500

    Fix

--- server/routes/router.js
+++ server/routes/router.js
@@ -9,6 +9,14 @@ router.use(
         origin: ['http://localhost:3000','https://honda-e-commerce.vercel.app', 'https://enthusiastic-puce-dove.cyclic.app/cars' ]
     })
 )
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://honda-e-commerce.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    next();
+});
 router.get('/',nothing )
 router.post('/register', registerUser)
 router.post('/login', loginUser)
