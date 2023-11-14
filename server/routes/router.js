cf4058ce2d9314bc66f2957789032286679f09ac
MarkTocino <Mark_tocino@yahoo.com>
2023-11-14 02:47:05 -0500

    Fix

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.use(
     cors({
         credentials:true,
-        origin: ['http://localhost:3000','https://honda-e-commerce.vercel.app/']
+        origin: ['*']
     })
 )
 router.get('/',nothing )
